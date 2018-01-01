964e924c1dc97accb086193d9f21d9c103e0467e
Danko Lučić <dlucic33@gmail.com>
2018-01-01 20:45:00 +0100

    test that multiple calls move corresponding modal content correctly

--- test/modalbox.test.js
+++ test/modalbox.test.js
@@ -38,7 +38,7 @@ describe('modal box', () => {
   });
 
   it('should move everything from second selector parameter to created div', () => {
-    const $stuffToDisplayInModalbox = $('<div>').prop('id', 'someStuff').appendTo('body');
+    $('<div>').prop('id', 'someStuff').appendTo('body');
     const $button = $('<button>').prop('id', 'myButton').appendTo('body');
 
     createModal('#myButton', '#someStuff');
@@ -49,7 +49,28 @@ describe('modal box', () => {
   });
 
   context('when two modalboxes on same page', () => {
-    it('should move each content to corresponding modalbox div');
+    let button1;
+    let button2;
+
+    beforeEach(() => {
+      $('<div>').prop('id', 'someStuff1').appendTo('body');
+      $('<div>').prop('id', 'someStuff2').appendTo('body');
+      assert.equal($('body > #someStuff1').length, 1);
+      assert.equal($('body > #someStuff2').length, 1);
+      $button1 = $('<button>').prop('id', 'myButton1').appendTo('body');
+      $button2 = $('<button>').prop('id', 'myButton2').appendTo('body');
+    });
+
+    it('should move each content to corresponding modalbox div', () => {
+      createModal('#myButton1', '#someStuff1');
+      createModal('#myButton2', '#someStuff2');
+
+      assert.equal($('body > #someStuff1').length, 0);
+      assert.equal($('body > #someStuff2').length, 0);
+      assert.equal($('.js-modal-box-content-div').eq(0).find('#someStuff1').length, 1);
+      assert.equal($('.js-modal-box-content-div').eq(1).find('#someStuff2').length, 1);
+    });
+
     it('should attach different event handlers for each');
   });
 });
